37b954c61dace046b181ba7087a3f367dd2afe08
aam198 <aam198@psu.edu>
2022-01-03 15:26:12 -0500

    Animation fix, hide check/fix

--- src/main.js
+++ src/main.js
@@ -3,20 +3,35 @@ const expandBtn = document.getElementById("expandBtn");
 const cardOne = document.getElementById("card-one");
 const cardTwo = document.getElementById("card-two");
 const compareContainer = document.querySelector(".compare-container");
+const wrapper=document.querySelector(".wrapper");
 
 
 function sideBySide() {
-  if(cardTwo.classList.contains('expand-Img-2') || cardOne.classList.contains('expand-Img-1')) {
+
+  wrapper.style.padding= 1.5 + "rem";
+  const hideCheck1 = cardOne.classList;
+  const hideCheck2 = cardTwo.classList;
+
+  if(hideCheck1.contains('hidden')){
+    hideCheck1.remove('hidden');
+  }
+
+  if(hideCheck2.contains('hidden')){
+    hideCheck2.remove('hidden');
+  }
+
+  if(cardTwo.classList.contains('expand-Img-2') ||      cardOne.classList.contains('expand-Img-1')) {
     cardOne.classList.toggle('expand-Img-1');
     cardTwo.classList.toggle('expand-Img-2');
     compareContainer.style.display="block";
     toggleBtn.disabled=false;
     expandBtn.innerHTML="Expand";
+    cardTwo.classList.add('hidden');
   }
-  else{
+  else {
     expandBtn.innerHTML="Collapse";
-    cardOne.classList.toggle('expand-Img-1');
-    cardTwo.classList.toggle('expand-Img-2');
+    cardOne.classList.add('expand-Img-1');
+    cardTwo.classList.add('expand-Img-2');
   }
 }
 
